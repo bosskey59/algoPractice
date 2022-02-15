bc824e92417650d702ada38c0e30c84ed3c71d24
Alejandro Aguilar Jr <a.a.j@live.com>
2022-02-14 22:58:25 -0600

    bestTime to buy done

---
+++ bestTimeToBuy.js
@@ -0,0 +1,23 @@
+var maxProfit = function (prices) {
+  let min = prices[0];
+  let max = 0;
+
+  prices.forEach((price) => {
+    if (price < min) {
+      min = price;
+    }
+    let diff = price - min;
+    if (diff > max) {
+      max = diff;
+    }
+  });
+  return max;
+};
+
+// quick solution loop once to find lowest price
+// loop again to find greatest after that index.
+const prices = [7, 1, 5, 3, 6, 4];
+const prices2 = [7, 6, 4, 3, 1];
+const prices3 = [2, 4, 1];
+
+console.log(maxProfit(prices3));
